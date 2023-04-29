1771c18559879dfe283798316cff9d2f4e615951
charithajay <charithajayathilaka1997@gmail.com>
2023-04-29 19:43:02 +0530

    adding a comment

--- my-npm-project/my-first-node-code.js
+++ my-npm-project/my-first-node-code.js
@@ -4,3 +4,6 @@ let input_text = "Node Express Tutorial";
 let ascii_text = ascii_text_generator(input_text, "3");
 
 console.log(ascii_text);
+
+
+// Output: will be Node Express Tutorial word with large size with word space
