5976bfea784130754bd8136147d03ec0c8a247ab
charithajay <charithajayathilaka1997@gmail.com>
2023-04-29 19:38:09 +0530

    commiting all the files including package manager

---
+++ my-npm-project/my-first-node-code.js
@@ -0,0 +1,6 @@
+let ascii_text_generator = require("ascii_text_generator");
+
+let input_text = "Node Express Tutorial";
+let ascii_text = ascii_text_generator(input_text, "3");
+
+console.log(ascii_text);
